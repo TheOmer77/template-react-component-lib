9b49f676398c8b01f2bf94e1ab19eed119178f1b
Omer <27678413+TheOmer77@users.noreply.github.com>
2023-02-24 22:22:41 +0200

    Add testing stuff

---
+++ jest.config.ts
@@ -0,0 +1,10 @@
+import type { Config } from 'jest';
+
+const config: Config = {
+  testEnvironment: 'jsdom',
+  moduleNameMapper: {
+    '.(css|less|scss)$': 'identity-obj-proxy',
+  },
+};
+
+export default config;
