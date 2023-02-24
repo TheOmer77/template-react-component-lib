b4e94bdf41f389b27ab55d5e0b80eb49a3cb1fa0
Omer <27678413+TheOmer77@users.noreply.github.com>
2023-02-25 00:12:04 +0200

    Add optimization stuff

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -4,6 +4,8 @@ import commonjs from '@rollup/plugin-commonjs';
 import typescript from '@rollup/plugin-typescript';
 import dts from 'rollup-plugin-dts';
 import postcss from 'rollup-plugin-postcss';
+import terser from '@rollup/plugin-terser';
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 
 import pkg from './package.json' assert { type: 'json' };
 
@@ -15,10 +17,12 @@ const config = defineConfig([
       { file: pkg.module, format: 'esm', sourcemap: true },
     ],
     plugins: [
+      peerDepsExternal(),
       resolve(),
       commonjs(),
       typescript({ tsconfig: './tsconfig.json' }),
       postcss(),
+      terser(),
     ],
   },
   {
