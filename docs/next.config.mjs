744868417df2920de4e6526278005d7fe8d26a35
Omer <27678413+TheOmer77@users.noreply.github.com>
2023-03-04 19:19:48 +0200

    Export docs as static files

--- docs/next.config.mjs
+++ docs/next.config.mjs
@@ -1,7 +1,11 @@
 import nextra from 'nextra';
 
 /** @type {import('next').NextConfig} */
-const nextConfig = { reactStrictMode: true, distDir: 'dist' };
+const nextConfig = {
+  reactStrictMode: true,
+  output: 'export',
+  images: { unoptimized: true },
+};
 
 const config = nextra({
   theme: 'nextra-theme-docs',
